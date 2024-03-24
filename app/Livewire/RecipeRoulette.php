60e3dc6fef187e68c14d768dc524f088b883465a
Tobias Kokesch <tobias@binary-hype.com>
2024-03-24 10:57:23 +0100

    🚧 Init Roulette Card

---
+++ app/Livewire/RecipeRoulette.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class RecipeRoulette extends Component
+{
+    public function render()
+    {
+        return view('livewire.recipe-roulette');
+    }
+}
---
+++ resources/views/livewire/recipe-roulette.blade.php
@@ -0,0 +1,18 @@
+<div class="max-w-2xl w-full px-3 my-3">
+    {{-- The Master doesn't talk, he acts. --}}
+    <div class="border-2 border-neutral rounded-lg">
+        <img class=""
+             src="https://images.unsplash.com/photo-1519708227418-c8fd9a32b7a2?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D">
+
+        <div class="p-5">
+            <ul class="grid grid-cols-2">
+                <li>1</li>
+                <li>1</li>
+                <li>1</li>
+                <li>1</li>
+                <li>1</li>
+                <li>1</li>
+            </ul>
+        </div>
+    </div>
+</div>
