ecdc73fa17b2e59d24967d8a0ec1c110ac6281a1
Tobias Kokesch <tobias@binary-hype.com>
2024-03-25 18:12:08 +0100

    🚧 sitemap

--- routes/console.php
+++ routes/console.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Console\Commands\DeleteUnusedFiles;
+use App\Console\Commands\GenerateSitemap;
 use Illuminate\Support\Facades\Schedule;
 
 Schedule::command(DeleteUnusedFiles::class)->daily()->withoutOverlapping();
+Schedule::command(GenerateSitemap::class)->daily()->withoutOverlapping();
