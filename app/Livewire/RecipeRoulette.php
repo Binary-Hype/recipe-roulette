f56bb6abd7580d7a8c7c9ca23dbe66516dd28aca
Tobias Kokesch <tobias@binary-hype.com>
2024-04-01 08:35:35 +0200

    🚧 remove comment

--- app/Livewire/RecipeRoulette.php
+++ app/Livewire/RecipeRoulette.php
@@ -16,11 +16,6 @@ class RecipeRoulette extends Component
         return view('livewire.recipe-roulette');
     }
 
-    //    public function mount()
-    //    {
-    //        $this->recipe = Recipe::where('type', $this->type)->inRandomOrder()->first() ?? null;
-    //    }
-
     public function fetchRandomRecipe()
     {
         $this->recipe = Recipe::where('type', $this->type)->inRandomOrder()->first() ?? null;
